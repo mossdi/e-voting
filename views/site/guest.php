118352582b11b4edf0a3563ab2b2521f0937d7d2
Мосолов Дмитрий Анатольевич <mossol_87@mail.ru>
2019-04-18 10:26:01 +0000

    Обновить 'views/site/guest.php'

--- views/site/guest.php
+++ views/site/guest.php
@@ -9,19 +9,14 @@ app\assets\VueGuestAsset::register($this);
 
 <div id="appGuest">
     <template v-if="speechConcluding">
-        <?= $this->render(
-            '/html_block/speech_concluding'
-        ) ?>
+        <?= $this->render('/html_block/speech_concluding') ?>
     </template>
     <template v-else-if="nowSpeech">
-        <?= $this->render(
-            '/html_block/speech_now'
-        ) ?>
+        <?= $this->render('/html_block/speech_now') ?>
     </template>
     <template v-else>
         <?= $this->render('/html_block/speech_list', [
-                'speechList' => $speechList,
-            ]
-        ) ?>
+        	'speechList' => $speechList,
+       	]) ?>
     </template>
 </div>
